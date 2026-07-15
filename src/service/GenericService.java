338cb7b2e3e75c5e75102f8bae0800547a6e56e9
beykod <alirafibeyk@gmail.com>
2026-07-15 12:08:27 +0330

    feat: add 3 methods to GenericService to handle some problems

--- src/service/GenericService.java
+++ src/service/GenericService.java
@@ -1,9 +1,20 @@
 package service;
 
+import model.Event;
+
 import java.math.BigDecimal;
 
-public interface GenericService<T>{
+public interface GenericService<T> {
     // a method to save and update :
-        void Check(T ch);
+    int checkForSaveAndUpdate(T ch);
+
+    //to control delete methods :
+    int checkForDelete(T ch);
+
+    //a method to control fin by id :
+    int checkForFindById(T ch);
+
+    // a method to check for find all :
+    int checkForFindAll(T ch);
 
 }
